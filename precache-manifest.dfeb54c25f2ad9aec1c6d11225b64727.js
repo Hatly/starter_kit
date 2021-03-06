be18a19e3b555d374fe85bfa83a5bb20e552d621
Hatly <etmam.ecom@gmail.com>
2021-03-06 18:15:33 +0200

    Updates

---
+++ precache-manifest.dfeb54c25f2ad9aec1c6d11225b64727.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f4b5dc2b258eb87dc9d",
+    "url": "/HAT/static/css/main.fbd96dfc.chunk.css"
+  },
+  {
+    "revision": "2f4b5dc2b258eb87dc9d",
+    "url": "/HAT/static/js/main.2f4b5dc2.chunk.js"
+  },
+  {
+    "revision": "7b2d49048e90c276ae3c",
+    "url": "/HAT/static/css/1.ed02b254.chunk.css"
+  },
+  {
+    "revision": "7b2d49048e90c276ae3c",
+    "url": "/HAT/static/js/1.7b2d4904.chunk.js"
+  },
+  {
+    "revision": "141eeddedae5f91f616d",
+    "url": "/HAT/static/js/runtime~main.141eedde.js"
+  },
+  {
+    "revision": "8a6ae55726918abf8fbe6f18d2784ab4",
+    "url": "/HAT/static/media/token-logo.8a6ae557.png"
+  },
+  {
+    "revision": "616af37972dc7deaecef45a6dfa463c9",
+    "url": "/HAT/static/media/eth-logo.616af379.png"
+  },
+  {
+    "revision": "91dadcf59b20da6922866ba8e2ed879c",
+    "url": "/HAT/index.html"
+  }
+];
